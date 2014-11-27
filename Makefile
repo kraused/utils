9b919ee7cca72aebfe185eccd40507a27fb93205
Dorian Krause <d.krause@fz-juelich.de>
2014-11-27 20:37:12 +0100

    Add sanitize tool
    sanitize clears out whitespaces and unreadable characters from
    file names. Using the --md5sum switch it can also rename files
    to their md5sum.

--- Makefile
+++ Makefile
@@ -6,4 +6,5 @@ install:
 	install -m 500 now      	$(HOME)/bin
 	install -m 500 srm      	$(HOME)/bin
 	install -m 500 archive-imap.py 	$(HOME)/bin/archive-imap
+	install -m 500 sanitize.py 	$(HOME)/bin/sanitize
 
---
+++ sanitize.py
@@ -0,0 +1,31 @@
+#!/bin/env python
+
+import os
+import sys
+import re
+import string
+import argparse
+import hashlib
+
+
+parser = argparse.ArgumentParser(description = "Sanitize file names in a directory")
+
+parser.add_argument("--md5sum" , help = "Rename files to <md5sum of file>", action = "store_true")
+parser.add_argument("directory", help = "Directory to scan")
+
+args = parser.parse_args()
+
+for f in os.listdir(args.directory):
+
+	g = None
+	if args.md5sum:
+		tmp = hashlib.md5()
+		tmp.update(open("/".join([args.directory, f])).read())
+		g = tmp.hexdigest()
+	else:
+		g = filter(lambda x: x in string.printable, f.replace(' ', '_'))
+
+	assert(g)
+	if f != g:
+		os.rename("/".join([args.directory, f]), "/".join([args.directory, g]))
+
